98b694a92bef1d127cd08e0b30781fd9cb176952
weiwei cai <shiyybua@gmail.com>
2017-06-07 03:43:06 -0700

    todo DQN

--- RLs/DQN/Brain.py
+++ RLs/DQN/Brain.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
-
+import numpy as np
 
 class DQN:
-    def __init__(self, image_shape):
+    def __init__(self, net_name, image_shape=[210, 160, 3]):
         '''
         :param image_shape: [height, width, channel], (210, 160, 3)
         '''
         self.image_shape = image_shape
+        self.net_name = net_name
         self.observation = tf.placeholder(dtype=tf.float32, shape=[None] + self.image_shape) / 255.0
+        with tf.name_scope(net_name):
+            self._build_net()
 
     def _init_weight(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial)
+        return tf.Variable(initial, collections=[self.net_name, tf.GraphKeys.GLOBAL_VARIABLES])
 
     def _init_biases(self, shape):
         initial = tf.contants(0.1, shape=shape)
-        return tf.Variable(initial)
+        return tf.Variable(initial, collections=[self.net_name, tf.GraphKeys.GLOBAL_VARIABLES])
 
     def conv_layer(self, x, W, b, activation_func=tf.nn.relu):
         return activation_func(tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME') + b)
@@ -59,6 +62,15 @@ class DQN:
             fc2_W = self._init_weight([1024, 4])
             fc2_b = self._init_biases([4])
             self.net_ouput = self.fc_layer(fc1_ouput, fc2_W, fc2_b)
+
+
+class Brain():
+    def __init__(self):
+        self.Q_net = DQN("Q_net")
+        self.target_net = DQN("target_net")
+        self.memory = np.array()
+
+
 
 
 
--- RLs/DQN/Breakout.py
+++ RLs/DQN/Breakout.py
@@ -1,19 +1,43 @@
 import gym
+import tensorflow as tf
+import numpy as np
+from .Brain import DQN
 
 env = gym.make('Breakout-v0')
 env.seed(1)     # reproducible.
 env = env.unwrapped
+epoch = 1000
 
 print(env.action_space)
 print(env.observation_space)
 # print(env.observation_space.high)
 # print(env.observation_space.low)
 
-while True:
-    env.render()
-    action = 1
-    observation_, reward, done, info = env.step(action)
-    print observation_
-    print reward
-    break
+# while True:
+#     env.render()
+#     action = 1
+#     observation_, reward, done, info = env.step(action)
+#     print observation_
+#     print reward
+#     break
+
+Q_net = DQN("Q_net")
+target_net = DQN("target_net")
+memory = np.array()
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    for episode in range(epoch):
+        observation = env.reset()
+        while True:
+            # fresh env
+            env.render()
+
+            # # RL choose action based on observation
+            # action = RL.choose_action(observation)
+            #
+            # # RL take action and get next observation and reward
+            # observation_, reward, done = env.step(action)
+
+
 
