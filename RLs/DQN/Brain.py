3f12640d5200452a11119132d977914ac240745f
shiyybua <shiyybua@gmail.com>
2017-06-10 17:27:50 +0800

    todo saver

--- RLs/DQN/Brain.py
+++ RLs/DQN/Brain.py
@@ -30,6 +30,19 @@ class DQN:
         init = tf.global_variables_initializer()
         self.sess.run(init)
 
+    def variable_summaries(self, var):
+        """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+        with tf.name_scope('summaries'):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            tf.summary.scalar('stddev', stddev)
+            tf.summary.scalar('max', tf.reduce_max(var))
+            tf.summary.scalar('min', tf.reduce_min(var))
+            tf.summary.histogram('histogram', var)
+
+
     def _init_weight(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
         return tf.Variable(initial, collections=[self.net_name, tf.GraphKeys.GLOBAL_VARIABLES])
@@ -82,6 +95,10 @@ class DQN:
         with tf.name_scope("fc2"):
             fc2_W = self._init_weight([1024, 4])
             fc2_b = self._init_biases([4])
+
+            self.variable_summaries(fc2_W)
+            self.variable_summaries(fc2_b)
+
             # 把算出的值直接当Value值，则不能加softmax
             self.net_ouput = self.fc_layer(fc1_ouput, fc2_W, fc2_b, None)
 
@@ -90,6 +107,7 @@ class DQN:
             self.loss = tf.reduce_mean(tf.square(self.target_value - Q_action))
             optimizer = tf.train.RMSPropOptimizer(1e-6)
             self.train = optimizer.minimize(self.loss)
+
 
 
     def choose_action(self, observation):
@@ -104,7 +122,7 @@ class DQN:
         self.memory.append((observation, action, reward, _observation))
         self.steps += 1
 
-    def learn(self, target_net):
+    def learn(self, target_net, merged, train_writer):
         batch = random.sample(self.memory, self.batch_size)
         observation_batch = [t[0] for t in batch]
         action_batch = [t[1] for t in batch]
@@ -113,12 +131,19 @@ class DQN:
 
         q_next = self.sess.run(target_net.net_ouput, feed_dict={target_net.observation: next_observation_batch})
         y = reward_batch + self.gamma * np.max(q_next, axis=1)
-        self.sess.run(self.train, feed_dict={self.observation: observation_batch, self.target_value:y,
+        y = y.reshape([self.batch_size,1])
+
+        # convert action into one-hot representation
+        action_batch = np.eye(4)[action_batch]
+        _, summary= self.sess.run([self.train, merged], feed_dict={self.observation: observation_batch, self.target_value:y,
                                              self.action: action_batch})
 
         if self.steps % self.replace_target_iter == 0:
             translate = [tf.assign(e2, e1) for e1, e2 in zip(tf.get_collection('target_net'), tf.get_collection('Q_net'))]
             self.sess.run(translate)
+
+        tf.summary.scalar('reward', sum(reward_batch) * 1.0 / len(reward_batch))
+        train_writer.add_summary(summary, self.steps)
 
 
 
--- RLs/DQN/Breakout.py
+++ RLs/DQN/Breakout.py
@@ -12,22 +12,15 @@ epoch = 1000
 
 print(env.action_space)
 print(env.observation_space)
-# print(env.observation_space.high)
-# print(env.observation_space.low)
-
-# while True:
-#     env.render()
-#     action = 1
-#     observation_, reward, done, info = env.step(action)
-#     print observation_
-#     print reward
-#     break
 
 if __name__ == '__main__':
     with tf.Session() as sess:
         Q_net = DQN("Q_net",sess)
         target_net = DQN("target_net",sess)
+        train_writer = tf.summary.FileWriter('../../resource/DQN_record',
+                                             sess.graph)
         step = 0
+        merged = tf.summary.merge_all()
         for episode in range(epoch):
             observation = env.reset()
             while True:
@@ -38,7 +31,8 @@ if __name__ == '__main__':
                 Q_net.store_transition(observation,action,reward,observation_)
 
                 if (step > 200) and (step % 5 == 0):
-                    Q_net.learn()
+                    Q_net.learn(target_net, merged, train_writer)
+
 
                 step += 1
 
@@ -46,9 +40,5 @@ if __name__ == '__main__':
                     break
 
             print episode
-
-
-
-
 
 
