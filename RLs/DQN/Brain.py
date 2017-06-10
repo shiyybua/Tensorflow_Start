1db3a2158484e58d8e9f3e7e2b923fe13a86ab89
shiyybua <shiyybua@gmail.com>
2017-06-10 16:12:56 +0800

    basic done with bugs

--- RLs/DQN/Brain.py
+++ RLs/DQN/Brain.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*
+
 import tensorflow as tf
 import numpy as np
 from collections import deque
@@ -14,11 +16,13 @@ class DQN:
         self.image_shape = image_shape
         self.net_name = net_name
         self.observation = tf.placeholder(dtype=tf.float32, shape=[None] + self.image_shape) / 255.0
-        self.target_value = tf.placeholder(dtype=tf.float32, shape=[None, 4])
+        self.target_value = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+        self.action = tf.placeholder(dtype=tf.float32, shape=[None, 4])
         self.replace_target_iter = 100
         self.steps = 0
         self.batch_size = 64
-        self.gamma = 0.99
+        self.gamma = 0.9
+        self.epsilon = 0.9
 
         with tf.name_scope(net_name):
             self._build_net()
@@ -31,7 +35,7 @@ class DQN:
         return tf.Variable(initial, collections=[self.net_name, tf.GraphKeys.GLOBAL_VARIABLES])
 
     def _init_biases(self, shape):
-        initial = tf.contants(0.1, shape=shape)
+        initial = tf.constant(0.1, shape=shape)
         return tf.Variable(initial, collections=[self.net_name, tf.GraphKeys.GLOBAL_VARIABLES])
 
     def conv_layer(self, x, W, b, activation_func=tf.nn.relu):
@@ -42,7 +46,10 @@ class DQN:
                         strides=[1, 2, 2, 1], padding='SAME')
 
     def fc_layer(self, x, W, b, activation_func=tf.nn.relu):
-        return activation_func(tf.nn.xw_plus_b(x, W, b))
+        if activation_func is not None:
+            return activation_func(tf.nn.xw_plus_b(x, W, b))
+        else:
+            return tf.nn.xw_plus_b(x, W, b)
 
 
     def _build_net(self):
@@ -57,37 +64,41 @@ class DQN:
             conv2_W = self._init_weight([10,10,32,64])
             conv2_b = self._init_biases([64])
             conv2_output = self.conv_layer(conv1_output, conv2_W, conv2_b)
-            conv2_output = self.conv_layer(conv2_output)    # 53 * 40
+            conv2_output = self.max_pooling(conv2_output)    # 53 * 40
 
         with tf.name_scope("conv3"):
             conv3_W = self._init_weight([10,10,64,64])
             conv3_b = self._init_biases([64])
             conv3_output = self.conv_layer(conv2_output, conv3_W, conv3_b)
-            conv3_output = self.conv_layer(conv3_output)    # 27 * 20
+            conv3_output = self.max_pooling(conv3_output)    # 27 * 20
 
         with tf.name_scope("fc1"):
             conv3_flat = tf.reshape(conv3_output, [-1, 27*20*64])
             fc1_W = self._init_weight([27*20*64, 1024])
             fc1_b = self._init_biases([1024])
             fc1_ouput = self.fc_layer(conv3_flat, fc1_W, fc1_b)
-            fc1_ouput = tf.nn.dropout(fc1_ouput)
+            fc1_ouput = tf.nn.dropout(fc1_ouput,keep_prob=0.5)
 
         with tf.name_scope("fc2"):
             fc2_W = self._init_weight([1024, 4])
             fc2_b = self._init_biases([4])
-            self.net_ouput = self.fc_layer(fc1_ouput, fc2_W, fc2_b, tf.nn.softmax)
+            # 把算出的值直接当Value值，则不能加softmax
+            self.net_ouput = self.fc_layer(fc1_ouput, fc2_W, fc2_b, None)
 
         with tf.name_scope("loss"):
-            self.loss = tf.reduce_mean(tf.squared_difference(self.net_ouput, self.target_value))
-            optimizer = tf.train.RMSPropOptimizer()
+            Q_action = tf.reduce_sum(tf.multiply(self.net_ouput, self.action), axis=1)
+            self.loss = tf.reduce_mean(tf.square(self.target_value - Q_action))
+            optimizer = tf.train.RMSPropOptimizer(1e-6)
             self.train = optimizer.minimize(self.loss)
 
 
     def choose_action(self, observation):
-        # TODO: check
-        prob_weights = self.sess.run(self.self.net_ouput, feed_dict={self.observation: observation[np.newaxis, :]})
-        action = np.random.choice(range(prob_weights.shape[1]), p=prob_weights.ravel())
-        return action
+        prob_weights = self.sess.run(self.net_ouput, feed_dict={self.observation: observation[np.newaxis, :]})
+        prob_weights = prob_weights.ravel()
+        if np.random.uniform() < self.epsilon:
+            return np.argmax(prob_weights)
+        else:
+            return np.random.randint(0, len(prob_weights))
 
     def store_transition(self, observation, action, reward, _observation):
         self.memory.append((observation, action, reward, _observation))
@@ -100,26 +111,14 @@ class DQN:
         reward_batch = [t[2] for t in batch]
         next_observation_batch = [t[3] for t in batch]
 
-        q_next = self.sess.run(target_net.net_ouput, feed_dict={target_net.observation: observation_batch})
-
-
+        q_next = self.sess.run(target_net.net_ouput, feed_dict={target_net.observation: next_observation_batch})
+        y = reward_batch + self.gamma * np.max(q_next, axis=1)
+        self.sess.run(self.train, feed_dict={self.observation: observation_batch, self.target_value:y,
+                                             self.action: action_batch})
 
         if self.steps % self.replace_target_iter == 0:
             translate = [tf.assign(e2, e1) for e1, e2 in zip(tf.get_collection('target_net'), tf.get_collection('Q_net'))]
             self.sess.run(translate)
-
-
-
-
-class Brain():
-    def __init__(self):
-        self.Q_net = DQN("Q_net")
-        self.target_net = DQN("target_net")
-        self.memory = np.array()
-
-
-
-
 
 
 
--- RLs/DQN/Breakout.py
+++ RLs/DQN/Breakout.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*
+import sys
+sys.path.append('/Users/mac/PycharmProjects/Tensorflow_Start')
 import gym
 import tensorflow as tf
-import numpy as np
-from .Brain import DQN
+from RLs.DQN.Brain import DQN
 
 env = gym.make('Breakout-v0')
 env.seed(1)     # reproducible.
@@ -21,27 +23,29 @@ print(env.observation_space)
 #     print reward
 #     break
 
+if __name__ == '__main__':
+    with tf.Session() as sess:
+        Q_net = DQN("Q_net",sess)
+        target_net = DQN("target_net",sess)
+        step = 0
+        for episode in range(epoch):
+            observation = env.reset()
+            while True:
+                # fresh env
+                env.render()
+                action = Q_net.choose_action(observation)
+                observation_, reward, done, _ = env.step(action)
+                Q_net.store_transition(observation,action,reward,observation_)
 
-with tf.Session() as sess:
-    Q_net = DQN("Q_net",sess)
-    target_net = DQN("target_net",sess)
-    step = 0
-    for episode in range(epoch):
-        observation = env.reset()
-        while True:
-            # fresh env
-            env.render()
-            action = Q_net.choose_action(observation)
-            observation_, reward, done = env.step(action)
-            Q_net.store_transition(observation,action,reward,observation_)
+                if (step > 200) and (step % 5 == 0):
+                    Q_net.learn()
 
-            if (step > 200) and (step % 5 == 0):
-                Q_net.learn()
+                step += 1
 
-            step += 1
+                if done:
+                    break
 
-            if done:
-                break
+            print episode
 
 
 
